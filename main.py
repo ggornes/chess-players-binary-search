335ddbdac868e65a2153c204de7957b23bb57ba0
ggornes <gerardogornes@gmail.com>
2020-08-19 00:00:11 +0800

    implemented binary search

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import csv
+import random
+
 from player import Player
 
 
-
-def read_data():
+def read_data(file):
+    """
+    Read a csv file and store data as a list of Player instances
+    :param file: csv file path ('data/chess-players-2.csv')
+    :return: players: list of players read from file
+    """
     players = []
 
-    with open('data/chess-players-2.csv', encoding="utf8", newline='') as csv_file:
+    with open(file, encoding="utf8", newline='') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
             last_name = row['Last name']
@@ -26,11 +32,21 @@ def read_data():
 
 
 def print_players(players):
+    """
+    print all players details
+    :param players:
+    :return: void
+    """
     for player in players:
         player.print_details()
 
 
 def bubble_sort(players):
+    """
+    Sorting algorithm of complexity O(n^2)
+    :param players:
+    :return:
+    """
     n = len(players)
 
     for i in range(n):
@@ -38,7 +54,7 @@ def bubble_sort(players):
 
         for j in range(n - i - 1):
             if players[j].__gt__(players[j + 1]):
-                players[j], players[j+1] = players[j+1], players[j]
+                players[j], players[j + 1] = players[j + 1], players[j]
                 already_sorted = False
 
         if already_sorted:
@@ -47,14 +63,59 @@ def bubble_sort(players):
     return players
 
 
-if __name__ == '__main__':
-    players = read_data()
-    # print_players(players)
+def binary_search(players, other_player):
+    """
+    Search a sorted array by repeatedly dividing the search interval in half
+    :param players: players list read from csv file
+    :param other_player: a list of players to see if each one belongs to the players list
+    :return: 'm' index on the players list if player is fond, -1 if player is not found
+    """
+    l = 0
+    h = len(players) - 1
+    c = 0
 
+    while l <= h:
+        m = l + (h - l) // 2
+
+        if players[m].__eq__(other_player):
+            print('Number of tires: ' + str(c))
+            return m
+
+        elif players[m].__lt__(other_player):
+            l = m + 1
+
+        else:
+            h = m - 1
+        c += 1
+
+    print('Number of tires: ' + str(c))
+    return -1
+
+
+if __name__ == '__main__':
+    """ Read csv files """
+    players = read_data('data/chess-players-2.csv')
+    other_players = read_data('data/chess-players-subset.csv')
+
+    """create a list of random chess players from players[] list"""
+    random_players = []
+    for i in range(0, 3):
+        n = random.randint(0, 500)
+        # print(n)
+        random_players.append(players[n])
+
+    """ Sort the chess players list """
     bubble_sort(players)
 
-    print_players(players)
+    """ Search for each player in random_players List """
+    for op in random_players:
 
+        print('------ Searching: ' + op.first_name + ' ' + op.last_name)
+        result = binary_search(players, op)
 
-
-
+        if result == -1:
+            print("Could not find player")
+        else:
+            # print(result)
+            print("Found Player")
+            players[result].print_details()
