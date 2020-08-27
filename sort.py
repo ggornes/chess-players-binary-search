2dba4faf88edadbd6d11172c6225be4f0c5fa0a7
ggornes <gerardogornes@gmail.com>
2020-08-27 09:34:25 +0800

    fixed bubble_sort

--- sort.py
+++ sort.py
@@ -8,7 +8,7 @@ def bubble_sort(a):
     for i in range(n):
         already_sorted = True
         for j in range(n - i - 1):
-            if a[j] < [j + 1]:
+            if a[j] > a[j + 1]:
                 a[j], a[j + 1] = a[j + 1], a[j]
                 already_sorted = False
         if already_sorted:
